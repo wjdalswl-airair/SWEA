6d81418d6c547a94a68c9d7630afa0b00bc98c77
Jeong Minji <jmj20021030@gmail.com>
2026-02-10 23:31:33 +0900

    함수 채우기

--- D3/5201_move_container.py
+++ D3/5201_move_container.py
@@ -1,32 +1,36 @@
-# import sys
-# sys.stdin = open('sample_input.txt','r')
-#
-# T = int(input())
-# for tc in range(1,T+1):
-#     N,M = map(int,input().split())
-#     W = list(map(int,input().split()))
-#     T = list(map(int,input().split()))
-#     arr = [[] for i in range(1<<N)]
-#
-#     for i in range(1<<N):
-#         for j in range(N):
-#             if i & (1<<j) :
-#                 arr[i].append(W[j])
-#
-#     w_list = list(map(sum,arr))
-#
-#     print(w_list)
-#
-#     s=0
-#     for j in range(M):
-#         Max = -21e8
-#         for i in range(len(w_list)):
-#             if T[j]>=w_list[i] and Max<w_list[i]:
-#                 if Max < w_list[i]:
-#                     Max = w_list[i]
-#         s+=Max
-#
-#     print(f"#{tc} {s}")
+import sys
+sys.stdin = open('sample_input.txt','r')
+
+T = int(input())
+for tc in range(1,T+1):
+    N,M = map(int,input().split())
+    W = list(map(int,input().split()))
+    T = list(map(int,input().split()))
+    arr = [[] for i in range(1<<N)]
+    T = sorted(T,reverse=True)
+
+    for i in range(1<<N):
+        for j in range(N):
+            if i & (1<<j) :
+                arr[i].append(W[j])
+
+    def container(weight):
+        Min = 21e8
+        for i in range(len(arr)):
+            if sum(arr[i]) <= weight:
+                if Min > weight - sum[arr[i]]:
+                    Min = weight - sum[arr[i]]
+                    Sort = arr[i]
+        for i in Sort:
+            W.remove[i]
+        return Min
+
+    s=0
+    for i in range(len(T)):
+        if len(W) >0:
+            s += container(T[i])
+
+    print(f"#{tc} {s}")
 #
 a = [5,4,3,2,1]
 b = [5,2,1]
